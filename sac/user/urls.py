ed86003809a7c8bdccb0f1e84e8cd17e72e1eaa2
sarath7598 <sarathchandraadhi@gmail.com>
2022-08-18 14:48:19 +0530

    Test Submission

---
+++ sac/user/urls.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.urls import path
+from user import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('',views.home, name='home'),
+    path('register',views.register, name='register'),
+    path('login',views.login, name='login'),
+    path('about',views.fb, name='facebook')    
+]
